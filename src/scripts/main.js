dfe941774fc3301ae6f724237b7ace4dc6fb8f6b
Daryna Hnidash <melissa998877@gmail.com>
2023-10-31 15:17:56 +0200

    landing page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const blocks = document.querySelectorAll('.animated');
+
+  function checkBlocksVisibility() {
+    const windowHeight = window.innerHeight;
+
+    blocks.forEach(block => {
+      const blockPosition = block.getBoundingClientRect().top;
+
+      if (blockPosition < windowHeight - 100) {
+        block.style.opacity = '1';
+        block.style.transform = 'translate(0)';
+      }
+    });
+  }
+
+  checkBlocksVisibility();
+
+  window.addEventListener('scroll', function() {
+    checkBlocksVisibility();
+  });
+});
